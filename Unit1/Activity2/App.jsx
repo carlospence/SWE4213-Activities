4d604d02052543fbc02ae3b2b53a43b02d248c93
carlospence <carlospence@gmail.com>
2026-01-12 00:32:51 +0100

    Added Unit 1 Files

---
+++ Unit1/Activity2/App.jsx
@@ -0,0 +1,15 @@
+import Form from "./components/Form"
+import "./App.css"
+
+function App() {
+
+  return (
+    <>
+      <h1> SWE4213 Activity 2: </h1>
+      {/* TODO 8: Insert the form here */}
+
+    </>
+  )
+}
+
+export default App
---
+++ Unit1/Activity2/MyButton.jsx
@@ -0,0 +1,6 @@
+
+export default function MyButton({ onClick }) {
+  return (
+    // TODO 5: Create button component with the correct onClick that says "Submit Form"
+  );
+}
