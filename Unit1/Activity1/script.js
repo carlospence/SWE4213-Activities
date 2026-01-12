2430bc4bbf3dd27d28daa165b21220bcb5e29687
carlospence <carlospence@gmail.com>
2026-01-12 01:03:09 +0100

    Activity 1 Completed

--- Unit1/Activity1/script.js
+++ Unit1/Activity1/script.js
@@ -1,14 +1,17 @@
 const btn = document.getElementById("newImageBtn");
 const img = document.getElementById("photo");
+const label = document.getElementById("imageCountLabel");
 
 // TODO 4: Create a reference to the imageCountLabel
 
 
-let imageCount = 0; 
+let imageCount = 0;
 
 btn.addEventListener("click", () => {
   img.src = `https://picsum.photos/600/400?random=${Date.now()}`;
 
   // TODO 5: Update the image count label to the number of images that have been shown. 
-
+  imageCount++;
+  label.textContent = `Number of Images Shown: ${imageCount}`;
+  console.log("charles")
 });
